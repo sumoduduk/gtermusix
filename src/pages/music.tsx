935aa5c3d51d1d37e27e2d18a66fc9be7917efc3
sumoduduk <sumoduduk13@gmail.com>
2024-08-20 03:19:40 +0700

    feat: integrate js-api player in browser

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,32 +1,16 @@
-import { music_list } from '@/components/Sidebar';
 import { Button } from '@/components/ui/button';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import LiteYoutubeEmbed from 'react-lite-youtube-embed';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+interface YTEvent {
+    target: YTPlayer;
+}
 
 const Music = () => {
-    const myRef = useRef<HTMLIFrameElement>(null);
-
-    const { musicId } = useParams();
-
-    const play = () => {
-        if (myRef) {
-            const curr = myRef.current;
-            if (!curr) return;
-
-            // curr.play()
-        }
-    };
-
-    const title = musicId ? music_list[musicId] : '';
-
-    if (!musicId) {
-        return (
-            <div className='w-2/3 full flex '>
-                <h1 className='m-auto text-2xl'>GTermusix</h1>
-            </div>
-        );
-    }
+    const myRef = useRef<HTMLIFrameElement | null>(null);
+    const playerRef = useRef<YTPlayer | null>(null);
+    const [Playing, setPlaying] = useState(true);
 
     useEffect(() => {
         const button = document.querySelector('.lty-playbtn');
@@ -34,31 +18,111 @@ const Music = () => {
         if (!button || button == null) return;
         const btnElm = button as HTMLButtonElement;
 
-        function createObserver() {
-            let observer;
+        let options = {
+            rootMargin: '-50%',
+            threshold: 1,
+        };
 
-            let options = {
-                rootMargin: '-50%',
-                threshold: 1,
+        let observer = new IntersectionObserver(() => {
+            return setTimeout(() => {
+                btnElm.click();
+            });
+        }, options);
+        observer.observe(btnElm);
+
+        const onPageLoad = () => {
+            const loadYouTubeIframeAPI = () => {
+                const tag = document.createElement('script');
+                tag.src = 'https://www.youtube.com/iframe_api';
+                tag.defer = true;
+                tag.async = true;
+                const firstScriptTag =
+                    document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
+
+                console.log('loadYouTubeIframeAPI');
             };
 
-            observer = new IntersectionObserver(() => btnElm.click(), options);
-            observer.observe(btnElm);
+            if (!window.YT) loadYouTubeIframeAPI();
+
+            window.onYouTubeIframeAPIReady = () => {
+                if (!myRef.current) return;
+                playerRef.current = new window.YT.Player(myRef.current, {
+                    events: {
+                        onReady: (event: YTEvent) => {
+                            event.target.playVideo();
+                        },
+                    },
+                });
+                console.log(playerRef.current);
+            };
+        };
+
+        if (document.readyState === 'complete') {
+            onPageLoad();
+            return;
+        } else {
+            window.addEventListener('load', onPageLoad);
         }
-        return createObserver();
+
+        return () => {
+            observer.unobserve(btnElm);
+            window.removeEventListener('load', onPageLoad);
+        };
     }, []);
+
+    const toggleVideo = () => {
+        if (!playerRef.current) return;
+        Playing
+            ? playerRef.current.pauseVideo()
+            : playerRef.current.playVideo();
+        setPlaying((v) => !v);
+    };
+
+    const nextVid = () => {
+        if (!playerRef.current) return;
+        playerRef.current.nextVideo();
+    };
+
+    const prevVideo = () => {
+        if (!playerRef.current) return;
+        playerRef.current.previousVideo();
+    };
+
+    const shuffle = () => {
+        if (!playerRef.current) return;
+        playerRef.current.setShuffle(true);
+        const arr = playerRef.current.getPlaylist();
+        console.log(arr);
+    };
+
+    const unFocusIframe = () => {
+        const iframe = document.querySelector<HTMLIFrameElement>(
+            '.hero__video-player iframe',
+        );
+        iframe?.setAttribute('tabindex', '-1');
+    };
 
     return (
         <div className='w-2/3 h-full flex flex-col gap-5'>
-            <h2 className='mx-auto'>{title}</h2>
+            {/* <h2 className='mx-auto'>{title}</h2> */}
 
-            <Button onClick={play}> Play</Button>
+            <div className='flex justify-center items-center'>
+                <Button onClick={prevVideo}>Previous Video</Button>
+                <Button onClick={nextVid}>Next Video</Button>
+            </div>
+
+            <Button onClick={toggleVideo}>Play Video</Button>
+            <Button onClick={shuffle}>Shuffle Playlist</Button>
             <LiteYoutubeEmbed
-                id={musicId}
-                title={title}
+                id='PL8noWinfnxi2uEgElXejICKtZY0_sAYK5'
+                title='Song List'
                 ref={myRef}
                 noCookie={true}
-                poster='default'
+                onIframeAdded={unFocusIframe}
+                params='enablejsapi=1'
+                // poster='default'
+                playlist={true}
                 adNetwork={false}
             />
 
