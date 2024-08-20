7e595563555ff8da2bc85c36d9f16fe470f3d03b
sumoduduk <sumoduduk13@gmail.com>
2024-08-20 18:47:53 +0700

    feat: add context, playlist_store and music_store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
-import { invoke } from '@tauri-apps/api/tauri';
+// import { useState } from 'react';
+// import { invoke } from '@tauri-apps/api/tauri';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 
+import localforage from 'localforage';
+
+type ContextType = {
+    musicStore: LocalForage;
+    playlistStore: LocalForage;
+    playlistNameStore: LocalForage;
+};
+
 function App() {
-    const [greetMsg, setGreetMsg] = useState('');
-    const [name, setName] = useState('');
+    // const [greetMsg, setGreetMsg] = useState('');
+    // const [name, setName] = useState('');
+
+    const musicStore = localforage.createInstance({
+        name: 'music_store',
+    });
+
+    const playlistNameStore = localforage.createInstance({
+        name: 'playlist_name_store',
+    });
+
+    const playlistStore = localforage.createInstance({
+        name: 'playlist_store',
+    });
 
     // async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -14,11 +34,23 @@ function App() {
     // }
 
     return (
-        <div className='flex w-screen bg-stone-900 text-gray-100'>
-            <Sidebar />
-            <Outlet />
+        <div className='flex w-screen h-screen bg-stone-900 text-gray-100'>
+            <Sidebar playlistStore={playlistStore} />
+            <Outlet
+                context={
+                    {
+                        musicStore,
+                        playlistStore,
+                        playlistNameStore,
+                    } satisfies ContextType
+                }
+            />
         </div>
     );
+}
+
+export function useStore() {
+    return useOutletContext<ContextType>();
 }
 
 export default App;
