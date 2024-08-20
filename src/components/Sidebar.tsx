20bd949fbe7625f1aba001903779513ebc2d2cfa
sumoduduk <sumoduduk13@gmail.com>
2024-08-20 18:46:05 +0700

    feat: pas playlist store to and add playlist name state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({ playlistStore }: { playlistStore: LocalForage }) => {
+    const [playlistName, setPlaylistName] = useState<Array<string>>([]);
+
+    const getPl = async () => {
+        let plKeys = await playlistStore.keys();
+        if (plKeys.length === 0) return;
+
+        const plNames = await Promise.all(
+            plKeys.map(async (key) => {
+                try {
+                    const name = await playlistStore.getItem(key);
+                    if (typeof name == 'string') {
+                        return name;
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            }),
+        );
+
+        const filtered = plNames.filter((nm) => nm !== undefined);
+
+        setPlaylistName(filtered);
+    };
+
+    useEffect(() => {
+        getPl();
+    }, []);
+
     return (
         <div className='w-1/3 h-screen overflow-scroll border-white border'>
             <h1>Songlist</h1>
-            <Link to='/music'>Go to Music</Link>
+            <Link to='/add-playlist'>+ Add Playlist</Link>
+            <hr />
+
+            {playlistName.length > 0 && (
+                <ul>
+                    {playlistName.map((plName) => (
+                        <li>plName</li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
